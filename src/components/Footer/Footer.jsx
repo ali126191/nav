883d618ce9a7adf1dff9d908c7841d63ee1de276
shrivitthal <shrivitthalhindumandir@gmail.com>
2019-04-19 18:09:40 -0600

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,43 +26,47 @@ function Footer({ ...props }) {
   });
  
   return (
-    <footer className="footer">
-          <Link to={"/login-page"} className="col" activeClassName="active">
-            <span>
-              <AccessAlarmIcon/>
-            </span>
-            <p id="text">Docs</p>
-          </Link>
-          <Link to={"/login-page"} className="col" activeClassName="active">
-            <span>
-              <AccessAlarmIcon/>
-            </span>
-            <p id="text">Docs</p>
-          </Link>
-          <Link to={"/landing-page"} className="col" activeClassName="active">
-            <span>
-             <AccessAlarmIcon/>
-            </span>            
-            <p>Tutorial</p>
-          </Link>
-          <Link to={"/profile-page"} className="col" activeClassName="active">
-            <span>
-              <AccessAlarmIcon/>
-            </span>
-            <p>plugins</p>
-          </Link>
-          <Link to={"/new"} className="col" activeClassName="active">
-            <span>
-              <AccessAlarmIcon/>
-            </span>           
-            <p>Blog</p>
-          </Link>
-          <Link to={"/new1"} className="col" activeClassName="active">
-            <span>
-              <AccessAlarmIcon/>
-            </span>
-            <p>Showcase</p>
-          </Link>
+    <footer className="footer">   
+      <Link to={"/login-page"} className="col" activeClassName="active">
+        <span>
+          <SvgIcon>
+            <path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+          </SvgIcon>  
+        </span>
+        <p id="text">Docs</p>
+      </Link>
+      <Link to={"/landing-page"} className="col" activeClassName="active">
+        <span>
+          <SvgIcon>
+            <path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+          </SvgIcon>  
+        </span>            
+        <p>Tutorial</p>
+      </Link>
+      <Link to={"/profile-page"} className="col" activeClassName="active">
+        <span>
+          <SvgIcon>
+            <path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+          </SvgIcon>  
+        </span>
+        <p>plugins</p>
+      </Link>
+      <Link to={"/new"} className="col" activeClassName="active">
+        <span>
+          <SvgIcon>
+            <path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+          </SvgIcon>  
+        </span>           
+        <p>Blog</p>
+      </Link>
+      <Link to={"/new1"} className="col" activeClassName="active">
+        <span>
+          <SvgIcon>
+            <path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" />
+          </SvgIcon>  
+        </span>
+        <p>Showcase</p>
+      </Link>
     </footer>
   );
 }
